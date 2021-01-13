994cf30b85d6f65b12e4e424dadcdc963156beb0
Angel Campos <acampos81@gmail.com>
2021-01-13 01:48:05 -0700

    Adding FisherYates implementation.

---
+++ GetRandomInt/FisherYates.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+namespace GetRandomInt
+{
+    public class FisherYates
+    {
+        private Random _rnd;
+        private int _index;
+        private List<int> _valueRange;
+        
+        public Action OnNumbersComplete;
+
+        public FisherYates(int min, int max)
+        {
+            _rnd = new Random();
+            InitValueRange(min, max);
+        }
+
+        public void AddCompleteCallback(Action callback)
+        {
+            OnNumbersComplete = callback;
+        }
+
+        public int GetRandomInt()
+        {
+            if (_index == _valueRange.Count)
+            {
+                _index = 0;
+                Shuffle(_valueRange);
+            }
+
+            return _valueRange[_index++];
+        }
+
+        private void InitValueRange(int min, int max)
+        {
+            _valueRange = new List<int>();
+
+            var count = (max - min)+1;
+
+            for (int i = 0; i < count; i++)
+                _valueRange.Add(min + i);
+
+            Shuffle(_valueRange);
+        }
+
+        // Fisher-Yates Shuffle with complexity of O(n)
+        // https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle#The_modern_algorithm
+        private void Shuffle(List<int> list)
+        {
+            if (OnNumbersComplete != null)
+                OnNumbersComplete();
+
+            var count = list.Count;
+            while (count > 1)
+            {
+                count--;
+                var i = _rnd.Next(count + 1);
+                var val = list[i];
+                list[i] = list[count];
+                list[count] = val;
+            }
+        }
+    }
+}
---
+++ GetRandomInt/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Diagnostics;
+
+
+namespace GetRandomInt
+{
+    class MainClass
+    {
+        public const int MIN_VALUE_LIMIT = 0;
+        public const int MAX_VALUE_LIMIT = 100000;
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Command: min max calls");
+            Console.WriteLine("Example: 1 10 20");
+            Console.WriteLine("1  - min");
+            Console.WriteLine("10 - max");
+            Console.WriteLine("20 - number of calls to GetRandomInt(min,max)");
+            Console.WriteLine();
+            Console.WriteLine("Enter command:");
+
+            string input;
+            while ((input = Console.ReadLine()) != null)
+            {
+                string[] command = input.Split(' ');
+                int min, max, calls;
+                int.TryParse(command[0], out min);
+                int.TryParse(command[1], out max);
+                int.TryParse(command[2], out calls);
+
+                min = min < MIN_VALUE_LIMIT ? MIN_VALUE_LIMIT : min;
+                max = max > MAX_VALUE_LIMIT ? MAX_VALUE_LIMIT : max;
+
+                // Insert better error checking and argument validation here
+
+                FisherYates randomizer = new FisherYates(min, max);
+                randomizer.OnNumbersComplete = HandleRandomNumbersComplete;
+
+                Console.WriteLine("----- Start -----");
+                var watch = Stopwatch.StartNew();
+                for (int i = 0; i < calls; i++)
+                {
+                    Console.WriteLine(randomizer.GetRandomInt());
+                }
+
+                watch.Stop();
+                Console.WriteLine("------ End ------");
+                Console.WriteLine(string.Format("Elapsed Time:{0} ms", watch.ElapsedMilliseconds));
+                Console.WriteLine();
+                Console.WriteLine("Enter command:");
+            }
+        }
+
+        private static void HandleRandomNumbersComplete()
+        {
+            Console.WriteLine("--- Random Numbers Complete ---");
+        }
+    }
+}
